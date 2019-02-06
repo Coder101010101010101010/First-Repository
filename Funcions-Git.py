0bd95e3939825f9024e62242c45785a329d34f74
Joe Hill <02joseph.hill@gulllakecs.org>
2019-02-06 13:07:38 -0500

    Added a greetings Function

---
+++ Funcions-Git.py
@@ -0,0 +1,12 @@
+# 4.13.3: Greetings
+# Mr. Lange
+# Joe Hill
+# 2.5.19
+
+name = input("What is your name: ")
+
+def greeting():
+    print ("Hi there " + name + "!")
+    print("Nice to meet you")
+
+greeting()
