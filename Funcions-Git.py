f53016c92ba9b7586312884888f7120b65632b29
Joe Hill <02joseph.hill@gulllakecs.org>
2019-02-27 12:58:56 -0500

    Name and Age

--- Funcions-Git.py
+++ Funcions-Git.py
@@ -37,3 +37,13 @@ def print_number(x):
 
 print_number(13)
 print_number(23)
+
+# 4.14.4: Name and Age
+# Joe Hill
+# 2.18.19
+
+def name_and_age(name, age):
+    print('\n','Hi, my name is', name, 'and I am', str(age), 'years old')
+
+name_and_age('Bob', 25)
+name_and_age('Man', '4,000,000,000')
