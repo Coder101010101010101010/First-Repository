3d3a1a72aaf15f7083eaf2714a90445bf4fc7893
Joe Hill <02joseph.hill@gulllakecs.org>
2019-02-28 12:07:32 -0500

    Defualt Parameter Values

--- Funcions-Git.py
+++ Funcions-Git.py
@@ -13,9 +13,9 @@ greeting()
 
 
 # 4.13.4: Functions and Variables
-# Mr. Lange
 # 2.11.19
 # Joe Hill
+
 
 x = 10
 
@@ -47,3 +47,16 @@ def name_and_age(name, age):
 
 name_and_age('Bob', 25)
 name_and_age('Man', '4,000,000,000')
+
+
+# 4.14.5: Defualt Parameter Values
+# Joe Hill
+# 2.19.19
+
+
+def print_two_numbers(x, y = 20):
+    print('First number:', x)
+    print('Second Number: ' + str(y))
+
+print_two_numbers(34, 45)
+print_two_numbers(78)
